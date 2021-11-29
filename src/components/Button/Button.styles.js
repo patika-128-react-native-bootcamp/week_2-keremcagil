ee53376cb27f067f25ed048a482d69f2738229ab
keremcagil <keremcagl@gmail.com>
2021-11-29 13:50:46 +0300

    homework

---
+++ src/components/Button/Button.styles.js
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  button: {
+    fontSize: 15,
+    padding: 5,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  btn: {
+    backgroundColor: '#204158',
+    alignItems: 'center',
+    margin: 5,
+    borderRadius: 5,
+  },
+});
